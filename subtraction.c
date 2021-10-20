7320434387ec07ae5fed2f11d0c89c08459c1589
Thireshsidda <92287626+Thireshsidda@users.noreply.github.com>
2021-10-20 10:19:40 +0530

    Add files via upload

---
+++ subtraction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+/*subtraction of two numbers*/
+{
+  int num1,num2,sub;
+  num1=18;
+  num2=7;
+  sub=num1-num2;
+  printf("sum of %d && %d is a %d",num1,num2,sub);  
+}
---
+++ variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+  int num1,num2,sum;
+  num1=9;
+  num2=11;
+  sum=num1+num2;
+  printf("sum of %d && %d is a %d",num1,num2,sum);  
+}
