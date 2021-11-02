2dc3717fa8e168028366500fb3e261b89dd8a351
Thireshsidda <92287626+Thireshsidda@users.noreply.github.com>
2021-11-02 10:38:47 +0530

    Add files via upload

---
+++ simple interest.c
@@ -0,0 +1,7 @@
+ #include<stdio.h>
+  int main()
+ { int p=10000,t=2;
+   float r=7.5,s_i;
+   s_i=p*t*r/100;
+   printf("Enter the principle=%d, time=%d, rate of interest=%f, simple interest=%f,",p,t,r,s_i);	
+ }
